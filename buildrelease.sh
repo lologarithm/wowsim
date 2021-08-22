085db0b6057f8a11533fe224c4219e107468b940
Ben Echols <lologarithm@gmail.com>
2021-08-21 18:47:09 -0600

    updated buildrelease to not include the lib.wasm

--- buildrelease.sh
+++ buildrelease.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm ./ui/lib.wasm
 GOOS=windows GOARCH=amd64 go build -o wowsim.exe web.go
 GOOS=darwin GOARCH=amd64 go build -o wowsim-amd64-darwin web.go
 GOOS=linux go build -o wowsim-amd64-linux web.go
+cd ui && GOARCH=wasm GOOS=js go build -o lib.wasm main_wasm.go
